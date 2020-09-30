e5bceac2eea970a3e3b4cf894318a9d94cdad1c6
gajanan15 <gajanandesai97@gmail.com>
2020-09-30 12:32:04 +0530

    [Gajanan]REFACTOR : Archive And Trash Component And Make UI Responsive

--- src/app/component/displaynotes/displaynotes.component.ts
+++ src/app/component/displaynotes/displaynotes.component.ts
@@ -1,79 +1,76 @@
 import { NoteDialogboxComponent } from './../note-dialogbox/note-dialogbox.component';
 import { FundooService } from './../../services/user_service/fundoo.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-displaynotes',
   templateUrl: './displaynotes.component.html',
-  styleUrls: ['./displaynotes.component.scss']
+  styleUrls: ['./displaynotes.component.scss'],
 })
 export class DisplaynotesComponent implements OnInit {
+  constructor(
+    private httpGetAllNote: FundooService,
+    public dialog: MatDialog
+  ) {}
 
-  constructor(private httpGetAllNote: FundooService,public dialog: MatDialog) { }
+  @Input() notes: [];
 
   list: any = [];
   noteList: any = [];
-  noteId:any=[];
-  archive:boolean;
-  notes:any;
-  title:any;
-  description:any;
+  noteId: any = [];
+  archive: boolean;
+  title: any;
+  description: any;
 
-  ngOnInit(): void {
-    this.getAllNote()
-  }
-  
-   openDialog(noteid) {
-      const dialogRef = this.dialog.open(NoteDialogboxComponent, {
-        width: '450px',
-        data: {noteId:noteid.id,title: noteid.title, description: noteid.description}
-      });
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-        this.getAllNote();
-        this.title = result;
-      });
-   }
-  
-  getAllNote() {
-    this.httpGetAllNote.getAllNotes().subscribe((response) => {
-      this.list = response;
-      this.noteList = this.list.data.data;
-      this.notes = this.noteList.filter((e)=>{
-        return e.isArchived === false && e.isDeleted === false
-      }).reverse();
-    }, error => {
-      console.log("Get Note Error: ", error)
-    })
+  ngOnInit(): void {}
+
+  openDialog(noteid) {
+    const dialogRef = this.dialog.open(NoteDialogboxComponent, {
+      width: '450px',
+      data: {
+        noteId: noteid.id,
+        title: noteid.title,
+        description: noteid.description,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 
-  addToArchive(noteid){
+  addToArchive(noteid) {
     this.noteId = [noteid.id];
     this.archive = noteid.isArchived;
-    var data={
-      isArchived:true,
-      noteIdList:this.noteId
+    var data = {
+      isArchived: true,
+      noteIdList: this.noteId,
     };
-    this.httpGetAllNote.addArchiveNote(data).subscribe((response)=>{
-      console.log("Archive Notes: ",response);
-      this.getAllNote();
-    },error=>{
-      console.log("Archive Error: ",error);
-    })
+    this.httpGetAllNote.addArchiveNote(data).subscribe(
+      (response) => {
+        console.log('Archive Notes: ', response);
+        // this.getAllNote();
+      },
+      (error) => {
+        console.log('Archive Error: ', error);
+      }
+    );
   }
 
-  addToTrash(noteid){
+  addToTrash(noteid) {
     this.noteId = [noteid.id];
-    var data={
-      isDeleted:true,
-      noteIdList:this.noteId
+    var data = {
+      isDeleted: true,
+      noteIdList: this.noteId,
     };
-    this.httpGetAllNote.addToTrash(data).subscribe((response)=>{
-      console.log("Trash API: ",response);
-      this.getAllNote();
-    },error=>{
-      console.log("Trash Error: ",error)
-    })
+    this.httpGetAllNote.addToTrash(data).subscribe(
+      (response) => {
+        console.log('Trash API: ', response);
+        // this.getAllNote();
+      },
+      (error) => {
+        console.log('Trash Error: ', error);
+      }
+    );
   }
 }
--- src/app/component/note/note.component.ts
+++ src/app/component/note/note.component.ts
@@ -1,15 +1,36 @@
+import { FundooService } from './../../services/user_service/fundoo.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrls: ['./note.component.scss']
+  styleUrls: ['./note.component.scss'],
 })
 export class NoteComponent implements OnInit {
+  constructor(private httpGetAllNote: FundooService) {}
 
-  constructor() { }
+  list: any = [];
+  noteList: any = [];
+  notes: any;
 
   ngOnInit(): void {
+    this.getAllNote();
   }
 
+  getAllNote() {
+    this.httpGetAllNote.getAllNotes().subscribe(
+      (response) => {
+        this.list = response;
+        this.noteList = this.list.data.data;
+        this.notes = this.noteList
+          .filter((e) => {
+            return e.isArchived === false && e.isDeleted === false;
+          })
+          .reverse();
+      },
+      (error) => {
+        console.log('Get Note Error: ', error);
+      }
+    );
+  }
 }
